f9388eadff7a29c6e74d64544ee492d1eec578ca
root <root@LAPTOP-1BTAU3S3>
2023-03-19 14:00:29 -0400

    Finished first,second

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [ans, setAnswer] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="IThinkThisCanBeAnything">
+                <Form.Label>
+                    Give Answer to the Ultimate Question of Life, the Universe,
+                    and Everything:
+                </Form.Label>
+                <Form.Control
+                    value={ans}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAnswer(event.target.value)
+                    }
+                />
+            </Form.Group>
+            {ans === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
