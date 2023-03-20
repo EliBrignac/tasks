0b2a3ed8fab83dfd500901f7e430d81df97d2798
EliBrignac <ebrignac@udel.edu>
2023-03-20 12:15:38 -0400

    All done task 10
    	modified:   src/form-components/ChangeColor.tsx
    	modified:   src/form-components/EditMode.tsx
    	modified:   src/form-components/MultipleChoiceQuestion.tsx

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [stu, setStudent] = useState<boolean>(true);
+    const [name, setName] = useState<string>("Your Name");
+    const [editMode, setEditMode] = useState<boolean>(false);
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="edit mode check"
+                label="edit mode"
+                inline
+                checked={editMode}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    setEditMode(event.target.checked)
+                }
+            />
+            <div>
+                <Form.Group controlId="editName">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                        value={name}
+                        hidden={!editMode}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setName(event.target.value)}
+                        disabled={!editMode}
+                    />
+                </Form.Group>
+            </div>
+            <div>
+                <Form.Check
+                    type="checkbox"
+                    id="is-student-check"
+                    label="student?"
+                    inline
+                    checked={stu}
+                    disabled={!editMode}
+                    hidden={!editMode}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setStudent(event.target.checked)
+                    }
+                />
+                {name} is {stu ? "" : "not"} a Student
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [ans, setAns] = useState<string>("");
+    function changeAns(event: React.ChangeEvent<HTMLSelectElement>): void {
+        setAns(event.target.value);
+    }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <h3> Multiple Choice Question</h3>
+            <Form.Group controlId="ChooseSomethinHomie">
+                <Form.Label>Choose Wisely🤺🦉🤔</Form.Label>
+                <Form.Select value={ans} onChange={changeAns}>
+                    {options.map((op: string) => (
+                        <option key={op} value={op}>
+                            {op}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {ans === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
